846e98e16721e97dfed17f8c0417b2d9beb3624e
CapitanLiteral <anaraguen@gmail.com>
2015-03-12 18:08:37 +0100

    a

---
+++ MyLibrary/ListNode.h
@@ -0,0 +1,20 @@
+template <class T> class ListNode
+{
+public:
+	ListNode(){}
+	~ListNode(){}
+	T val;
+	ListNode* next;
+	ListNode* previous;
+
+private:
+	
+};
+/*
+ListNode::ListNode()
+{
+}
+
+ListNode::~ListNode()
+{
+}*/
