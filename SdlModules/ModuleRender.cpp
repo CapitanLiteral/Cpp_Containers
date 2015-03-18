06755548e9f2d66d98a54f4e14d361755d601c7b
CapitanLiteral <anaraguen@gmail.com>
2015-03-18 12:02:33 +0100

    Added Project SDL

---
+++ SdlModules/ModuleRender.cpp
@@ -0,0 +1,11 @@
+#include "ModuleRender.h"
+
+
+ModuleRender::ModuleRender()
+{
+}
+
+
+ModuleRender::~ModuleRender()
+{
+}
---
+++ SdlModules/ModuleRender.h
@@ -0,0 +1,13 @@
+#ifndef __ModuleRender_H__
+#define __ModuleRender_H__
+
+#include "Module.h"
+
+class ModuleRender : public Module
+{
+public:
+	ModuleRender();
+	~ModuleRender();
+};
+
+#endif
